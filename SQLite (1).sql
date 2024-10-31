ab454c0bfab6cec3e3328810e907d8f59ec0c199
Dmitry Kharlamov <168891004+M0RECH0K@users.noreply.github.com>
2024-10-31 12:49:22 +0300

    Add files via upload

---
+++ SQLite (1).sql
@@ -0,0 +1,14 @@
+INSERT INTO customer(customer_name, cyti) VALUES('Dmitry', 'Pskov');
+INSERT INTO customer(customer_name, cyti) VALUES('Liana', 'NewYork');
+INSERT INTO customer(customer_name, cyti) VALUES('Anna', 'Moscow');
+INSERT INTO customer(customer_name, cyti) VALUES('Boris', 'Kazan');
+INSERT INTO customer(customer_name, cyti) VALUES('Vladimir', 'Ekaterinburg');
+
+
+INSERT INTO products(customer_id, products_name, amount, cost) VALUES(1, 'Cherry', 5.39, 0.19);
+INSERT INTO products(customer_id, products_name, amount, cost) VALUES(1, 'Cucumber', 9, 1.5);
+INSERT INTO products(customer_id, products_name, amount, cost) VALUES(2, 'Kefir', 14, 7);
+INSERT INTO products(customer_id, products_name, amount, cost) VALUES(3, 'Banana', 11.50, 2.30);
+INSERT INTO products(customer_id, products_name, amount, cost) VALUES(4, 'Rice', 4.99, 4.99);
+INSERT INTO products(customer_id, products_name, amount, cost) VALUES(4, 'Chicken', 6.95, 6.43);
+INSERT INTO products(customer_id, products_name, amount, cost) VALUES(5, 'Yogurt', 6.60, 2.20);
